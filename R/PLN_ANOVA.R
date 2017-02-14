24fd4a6153444c0511b1b9b4a05804db247c0618
zhanghfd <zhanghfd@fudan.edu.cn>
2017-02-14 11:44:10 +0800

    first commint

--- R/PLN_ANOVA.R
+++ R/PLN_ANOVA.R
@@ -1,17 +1,17 @@
 
 PLN_ANOVA = function(d,n.top=1e3){
 
-  R = d$conditionNumber;  
+  R = d$conditionNumber;
   count = d$count;
   ct = sweep(count,2,d$sample$sizeFactor,'/');
-  
+
   J = nrow(count);
   I = ncol(count)/R;
   y = array(NA,c(J,R,I));
   for(r in 1:R){
     y[,r,] = ct[,1:I+(r-1)*I]
   }
-  
+
   condition = as.factor(rep(1:R,each=I));
   p.value = rep(NA,J);
   for(j in 1:J){
@@ -20,20 +20,20 @@ PLN_ANOVA = function(d,n.top=1e3){
     p.value[j] = anova(fit)[1,5];
   }
   top.id = order(p.value)[1:n.top];
-  
+
   mu = apply(y,1:2,mean);
   etahat = log(mu);
   muhat = mean(etahat);
-  
+
   alphahat = colMeans(etahat) - muhat;
   betahat = rowMeans(etahat) - muhat;
   z = etahat - muhat - outer(betahat,alphahat,'+');
-  
+
   u1 = c(1,rep(-1/(R-1),R-1));
   u0 = u1 + 1;
   ep = 1e-6;
   z1 = z[top.id,];
-  
+
   if(R==2){
     uhat = c(1,-1);
     tmp = as.numeric(z%*%(uhat));
@@ -48,11 +48,11 @@ PLN_ANOVA = function(d,n.top=1e3){
     uhat = u1;
     vhat = as.numeric(z%*%(uhat))/sum(uhat^2);
   }
-  
-  
-  sigma = d$genewiseSigma;  
+
+
+  sigma = d$genewiseSigma;
   rho = d$rho;
-  
+
   var.v = rep(0,J);
   for(r1 in 1:R){
     for(r2 in 1:R){
@@ -64,15 +64,15 @@ PLN_ANOVA = function(d,n.top=1e3){
     }
   }
   var.v = var.v/sum(uhat^2)^2;
-  
+
   sd.v = sqrt(var.v);
   stat = (vhat/sd.v)^2;
-  
+
   th = qchisq(1-1e-5,1);
-  p.new = ifelse(stat<th,1-pchisq(stat,1),-pchisq(stat,1,log=TRUE));
-  log2fold.change = outer(as.numeric(vhat),as.numeric(uhat-uhat[1]))/log(2);
-  sd.log2foldchange = outer(as.numeric(sd.v),abs(as.numeric(uhat-uhat[1])))/log(2);
-  d$ANOVA = list(u=uhat,log2FoldChange=log2fold.change,sd.log2FoldChange=sd.log2foldchange,p.value=p.new));
-  
+  p.new = ifelse(stat<th,1-pchisq(stat,1),-pchisq(stat,1,log.p=TRUE));
+  logfold.change = outer(as.numeric(vhat),as.numeric(uhat-uhat[1]));
+  sd.logfoldchange = outer(as.numeric(sd.v),abs(as.numeric(uhat-uhat[1])));
+  d$ANOVA = list(u=uhat,logFoldChange=logfold.change,sd.logFoldChange=sd.logfoldchange,p.value=p.new);
+
   return(d);
 }
